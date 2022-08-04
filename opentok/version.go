c343efbba31bc1c5cdd5e929e19131233e2c8c98
Calvert <yz3210@gmail.com>
2022-08-04 17:13:41 +0800

    chore: bump version to v2.3.2 [skip ci]

--- opentok/version.go
+++ opentok/version.go
@@ -9,7 +9,7 @@ import (
 const sdkName = "opentok-go-sdk"
 
 // sdkVersion specifies the version of the SDK
-const sdkVersion = "2.3.1"
+const sdkVersion = "2.3.2"
 
 var (
 	userAgent = fmt.Sprintf("Go/%s (%s-%s) %s/%s",
